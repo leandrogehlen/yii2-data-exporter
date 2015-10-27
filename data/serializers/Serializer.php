d7d2f43e7c4a58ba13875711efcf7082b41bc7fe
leandrogehlen <leandrogehlen@gmail.com>
2015-10-27 17:38:56 -0200

    Refactoring

--- data/serializers/Serializer.php
+++ data/serializers/Serializer.php
@@ -3,7 +3,6 @@
 namespace leandrogehlen\exporter\data\serializers;
 
 use leandrogehlen\exporter\data\Column;
-use leandrogehlen\exporter\data\Dictionary;
 use leandrogehlen\exporter\data\Exporter;
 use leandrogehlen\exporter\data\Session;
 use yii\base\Object;
@@ -25,7 +24,7 @@ abstract class Serializer extends Object
     /**
      * Formats the specified session
      * @param Session $session
-     * @return array
+     * @return string
      */
     public function serialize($session)
     {
@@ -57,9 +56,9 @@ abstract class Serializer extends Object
         $value = ArrayHelper::getValue($row, $column->name, $column->value);
         $expression = $column->expression;
         $charComplete = $column->charComplete;
-        $align = $column->align;
         $size = $column->size;
         $format = $column->format;
+        $padding = $column->getPadding();
 
         $dictionary = $column->dictionaryName ? $this->exporter->findDictionary($column->dictionaryName) : null;
         if ($dictionary) {
@@ -69,8 +68,8 @@ abstract class Serializer extends Object
             if ($charComplete === null) {
                 $charComplete = $dictionary->charComplete;
             }
-            if ($align === null) {
-                $align = $dictionary->align;
+            if ($padding === null) {
+                $padding = $dictionary->getPadding();
             }
             if ($size === null) {
                 $size = $dictionary->size;
@@ -84,20 +83,8 @@ abstract class Serializer extends Object
             $value = $this->exporter->evaluate($expression, ['value' => $value]);
         }
 
-        switch ($align) {
-            case Dictionary::ALIGN_LEFT:
-                $align = STR_PAD_LEFT;
-                break;
-            case Dictionary::ALIGN_RIGHT:
-                $align = STR_PAD_RIGHT;
-                break;
-            case Dictionary::ALIGN_BOTH:
-                $align = STR_PAD_BOTH;
-                break;
-        }
-
         $value = (string) $value;
-        $value = ($size > strlen($value)) ? str_pad($value, $size, $charComplete, $align) : substr($value, 0, $size);
+        $value = ($size > strlen($value)) ? str_pad($value, $size, $charComplete, $padding) : substr($value, 0, $size);
 
         if ($format) {
             $value = $this->exporter->formatter->format($value, $format);
